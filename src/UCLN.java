b32055434e014bea82fba3c885331d10c9aea2d2
nndieu <nndieu88@gmail.com>
2019-09-07 12:00:47 +0700

    SS1

---
+++ src/UCLN.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class UCLN {
+    public static void main(String[] args) {
+        int a, b;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        a = sc.nextInt();
+        System.out.println("Enter number 2: ");
+        b = sc.nextInt();
+
+        a = Math.abs(a);
+        b = Math.abs(b);
+
+        if (a == 0 || b == 0) {
+            System.out.println("Ko co UCLN");
+        } else {
+            while (a != b) {
+                if (a > b) {
+                    a = a - b;
+                } else {
+                    b = b - a;
+                }
+            }
+            System.out.println("UCLN: " + a);
+        }
+    }
+}
